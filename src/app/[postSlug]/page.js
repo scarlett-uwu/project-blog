de8cab64155c2ef8e62b5ebacb47f81c13b13d13
scarlett.uwu <scarlett.uwu@kakaocorp.com>
2025-03-19 20:02:39 +0900

    🚀 Exercise 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,8 +1,18 @@
 import BlogHero from "@/components/BlogHero";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
+import { BLOG_TITLE } from "@/constants";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import styles from "./postSlug.module.css";
+
+export async function generateMetadata({ params }) {
+  const post = await loadBlogPost(params.postSlug);
+
+  return {
+    title: `${post.frontmatter.title} • ${BLOG_TITLE}`,
+    description: post.frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const post = await loadBlogPost(params.postSlug);
